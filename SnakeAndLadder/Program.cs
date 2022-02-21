8e6f09c456e789e6a89236f41f06a9a2e258f669
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-02-21 19:10:41 +0530

    First_use_case

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderApp;
+
+internal class SnakeAndLadder
+{
+    // Game related attributes declared here
+    int position;
+
+    // Default Constructor
+    public SnakeAndLadder()
+    {
+        position = 0;
+    }
+
+    public void Describe()
+    {
+        Console.WriteLine("Position: " + position);
+    }
+}
