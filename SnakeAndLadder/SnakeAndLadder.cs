67d344ff02680ce130acddfba6a16117149d2fa3
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-02-21 20:31:51 +0530

    Second_use_case

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using SnakeAndLadderApp;
+﻿using SnakeAndLadderApp;
 
 Console.Title = "Snake & Ladder";
 Console.WriteLine("------------------Snake and Ladder Simulator------------------");
 SnakeAndLadder player1 = new SnakeAndLadder();
+player1.Describe();
+player1.RollDie();
 player1.Describe();
 Console.ReadKey();
