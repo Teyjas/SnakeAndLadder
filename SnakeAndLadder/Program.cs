8b537af0a5d58af5e462edf03f7677e532b55217
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-02-22 12:36:29 +0530

    Third_use_case

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,7 +9,10 @@ namespace SnakeAndLadderApp;
 internal class SnakeAndLadder
 {
     // Game related attributes declared here
-    int position;
+    private int position;
+
+    // Common Random object for the class
+    static Random random = new Random();
 
     // Default Constructor
     public SnakeAndLadder()
@@ -17,8 +20,56 @@ internal class SnakeAndLadder
         position = 0;
     }
 
+    // Describes the players status in the game
     public void Describe()
     {
         Console.WriteLine("Position: " + position);
+    }
+
+    // Rolls die using Random
+    public void RollDie()
+    {
+        int roll = random.Next(1, 7);
+        Console.WriteLine("Rolled: " + roll);
+        position += roll;
+        Option(roll);
+        Describe();
+    }
+
+    // Checks option for player using Random
+    public void Option(int roll)
+    {
+        int option = random.Next(0, 3);
+        switch (option)
+        {
+            case 0:
+                Console.WriteLine("No Play");
+                break;
+            case 1:
+                Ladder(roll);
+                break;
+            case 2:
+                Snake(roll);
+                break;
+            default:
+                Console.WriteLine("Error!");
+                break;
+        }
+    }
+
+    // Actions to take when option is snake
+    private void Snake(int roll)
+    {
+        Console.WriteLine("Oops! Snake Trap!!!");
+        Console.WriteLine("Going down by " + roll);
+        position -= roll;
+    }
+
+    // Actions to take when option is ladder
+    private void Ladder(int roll)
+    {
+        Console.WriteLine("Yay! A Ladder!!!");
+        Console.WriteLine("Going up by " + roll);
+        position += roll;
     }
 }
--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -10,4 +10,5 @@ Console.Title = "Snake & Ladder";
 Console.WriteLine("------------------Snake and Ladder Simulator------------------");
 SnakeAndLadder player1 = new SnakeAndLadder();
 player1.Describe();
+player1.RollDie();
 Console.ReadKey();
